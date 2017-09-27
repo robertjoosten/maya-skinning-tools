23129ed69de76a1ff8a970634ba9c6bd5e25da52
Robert Joosten <rwm.joosten@gmail.com>
2017-09-27 23:23:34 +0100

    update documentation

--- smoothWeightsCtx/__init__.py
+++ smoothWeightsCtx/__init__.py
@@ -1,3 +1,42 @@
+"""					
+I N S T A L L A T I O N:
+    Copy the "rjSkinningTools" folder to your Maya scripts directory:
+        C:\Users\<USER>\Documents\maya\scripts
+        
+U S A G E:
+    Select a skinned object and run the following code:
+        from rjSkinningTools import smoothWeightsCtx;
+        smoothWeightsCtx.paint()
+    
+N O T E:
+    I would like to note that the idea for this script came from Tom Ferstl, 
+    he developed a script where the end results is very simular.
+
+    Profile: vimeo.com/tomferstl
+    Demo: vimeo.com/19802823
+
+    I did use the original script as an inspiration, but what happens under 
+    the hood is completely different.
+
+    artUserPaintCtx only likes to take in mel commands, but instead of 
+    writting a wrapper in mel that calls python code, I registered a new 
+    command which is written in python, improving the speed. This also gives 
+    you the option to add Undo and Redo functionality.
+
+    The paint tool calls a script that find the surrounding vertices and its 
+    skin weights. These skin weights are then blended with the skin weights 
+    of the original vertex based value of the paint tool.
+
+    It also takes into account the settings on your skinCluster. For real-time 
+    purposes its very common to have your maintain max influences set to a 
+    certain number. The same goes for normalization of the weights and the 
+    locked state of influences.
+"""
+
+__author__    = "Robert Joosten"
+__version__   = "0.9.0"
+__email__     = "rwm.joosten@gmail.com"
+
 from maya import cmds
 import os
 
